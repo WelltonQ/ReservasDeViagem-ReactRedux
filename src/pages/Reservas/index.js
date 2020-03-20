9cd89b767eaf72d58dc8b31899ad48754e1e7fcd
Wellton Quirino <welltonq@outlook.com>
2020-03-20 12:01:22 -0300

    Reservas de viagens com ReactJS e Redux - upload do projeto

---
+++ src/pages/Reservas/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import { MdDelete, MdRemoveCircle, MdAddCircle } from 'react-icons/md';
+
+import {removeReserve, updateAmountRequest} from '../../store/modules/reserve/actions';
+import './style.css';
+
+export default function Reservas() {
+  const dispatch = useDispatch();
+  const reserves = useSelector(state => state.reserve);
+
+  function handleRemove(id) {
+    dispatch(removeReserve(id))
+  }
+
+  function decAmount(trip) {
+    dispatch(updateAmountRequest(trip.id, trip.amount -1));
+  }
+
+  function incAmount(trip) {
+    dispatch(updateAmountRequest(trip.id, trip.amount +1));
+  }
+
+ return (
+   <div>
+    <h1 className="title">Você solicitou {reserves.length} reservas</h1>
+    
+    {reserves.map(reserve => (
+      <div className="reservas" key={reserve.id}>
+        <img
+          src={reserve.image}
+        alt={reserve.title}
+        />
+        <strong>{reserve.title}</strong>
+
+        <div id="amount">
+          <button type="button" onClick={() => decAmount(reserve)}>
+            <MdRemoveCircle size={25} color="#191919"/>
+          </button>
+          <input type="text" readOnly value={reserve.amount}/>
+          <button type="button" onClick={() => incAmount(reserve)}>
+            <MdAddCircle size={25} color="#191919"/>
+          </button>
+        </div>
+
+        <button
+        type="button"
+        onClick={()=> handleRemove(reserve.id)}
+        >
+          <MdDelete size={20} color="#191919" />
+        </button>
+      </div>
+    ))}
+
+    <footer>
+      <button type="button">Solicitar Reservas</button>
+    </footer>
+
+   </div>
+   
+ );
+}
